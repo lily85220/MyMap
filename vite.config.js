5f298518c642d9a6b26669a9d7699615bce74efc
Lily Chen <zz85220@gmail.com>
2023-04-17 10:27:38 +0800

    試看看import的時候引用相對路徑

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/MyMap/' : '/',
-  build: {
-    rollupOptions: {
-      entryFileNames: 'mapViewDeps.js',
-      chunkFileNames: 'mapViewDeps.js'
-    }
-  }
+  base: process.env.NODE_ENV === 'production' ? '/MyMap/' : '/'
 })
