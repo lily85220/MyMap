dbb9e3347bc35ffe20fd830622ed238ce8abc089
Lily Chen <zz85220@gmail.com>
2023-04-17 10:05:38 +0800

    更改config裡面的rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/MyMap/' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/MyMap/' : '/',
+  build: {
+    rollupOptions: {
+      entryFileNames: '[name].js',
+      chunkFileNames: '[name].js'
+    }
+  }
 })
